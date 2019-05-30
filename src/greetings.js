b87e9ea4ff275b23eacd19c71eac9d875ce29a2a
david kayembe <kayembe.david@gmail.com>
2019-05-30 10:31:30 +0100

    les fonctions des salutions ont été ajoutées dans le fichier greetings

---
+++ src/greetings.js
@@ -0,0 +1,4 @@
+export const greetings = (name) 
+    => console.log(`hello ${name}, how are you doing ?`);
+export const goodBye = (name, nextMeetingDate) 
+    => console.log(`good bye my friend ${name}, let's meet on ${nextMeetingDate}`);
