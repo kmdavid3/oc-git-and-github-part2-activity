3a4f045ae73d69161194d2dd8fafb603a3317cf1
david kayembe <kayembe.david@gmail.com>
2019-05-30 10:38:40 +0100

    the good bye function have been commented

--- src/greetings.js
+++ src/greetings.js
@@ -1,4 +1,5 @@
 export const greetings = (name) 
     => console.log(`hello ${name}, how are you doing ?`);
-export const goodBye = (name, nextMeetingDate) 
-    => console.log(`good bye my friend ${name}, let's meet on ${nextMeetingDate}`);
+
+// export const goodBye = (name, nextMeetingDate) 
+//     => console.log(`good bye my friend ${name}, let's meet on ${nextMeetingDate}`);
