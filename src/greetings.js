a12261c556463de7424db7b35f3943a83e0d6176
david kayembe <kayembe.david@gmail.com>
2019-05-30 10:43:11 +0100

    the greetings function have been modified, using now module.exports to export function

--- src/greetings.js
+++ src/greetings.js
@@ -1,5 +1,6 @@
-export const greetings = (name) 
-    => console.log(`hello ${name}, how are you doing ?`);
+module.exports = (name) => {
+    console.log(`hello ${name}, how are you doing ?`);
+};
 
 // export const goodBye = (name, nextMeetingDate) 
 //     => console.log(`good bye my friend ${name}, let's meet on ${nextMeetingDate}`);
